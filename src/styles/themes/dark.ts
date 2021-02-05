125ebc8a8b89aa98ef068fd25c6d37615a58df39
Paula Lopes <pls21@msn.com>
2021-02-05 12:17:54 -0300

    Theme Switch

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,12 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#431C52',
+    secundary: '#623C74',
+
+    background: '#825B97',
+    text: '#431C52'
+  },
+};
+
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,12 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#DDD5F3',
+    secundary: '#CEC2EB',
+
+    background: '#EBE8FC',
+    text: '#B19CD7'
+  },
+};
+
